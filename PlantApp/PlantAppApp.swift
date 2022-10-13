85df7a0663fdf7fda699da90fc29d64685e2e6d9
WillCatonJr <WillCatonJr@Gmail.com>
2022-10-12 20:09:25 -0500

    Initial Commit

---
+++ PlantApp/PlantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantAppApp.swift
+//  PlantApp
+//
+//  Created by Mini on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PlantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
